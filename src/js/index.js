89e011ef5ce890bbc6fbb62c1f6e3d1bd54546de
Roberto <robertopescee@hotmail.com>
2019-06-09 22:59:39 +0200

    initialize food2fork api

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,14 @@
-//install axios via npm
 import axios from 'axios';
 
 async function getResults(query) {
-    const proxy = 'www.exampleproxy.com/';
-    const key = 'authkeystring';
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = '8f73c7e564d493e7921299ac3c705b18';
     try {
-        const res = await axios(`${proxy}www.url.com/${key}`);
+        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
         const recipes = res.data.recipes;
         console.log(recipes);
     } catch (error) {
         alert(error);
     }
-};
-
+}
 getResults('pizza');
