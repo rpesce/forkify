625dd3b8a15882cff029a16dd3dba9060f254d4b
Roberto <robertopescee@hotmail.com>
2019-06-10 14:59:25 +0200

    Add Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,4 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '8f73c7e564d493e7921299ac3c705b18';
-    try {
-        const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
-    }
-}
-getResults('pizza');
+const search = new Search('pizza');
+console.log(search);
---
+++ src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '8f73c7e564d493e7921299ac3c705b18';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
