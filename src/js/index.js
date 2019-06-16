30ea546cce8d8470a83afa314017385092b964d6
Roberto <robertopescee@hotmail.com>
2019-06-16 15:26:25 +0200

    implement recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
-import * as searchView from './views/searchView'
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /*Global state of the app
@@ -62,6 +63,8 @@ const controlRecipe = async () => {
     
     if (id) {
         //prepre UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
         //Create new recipe object
         state.recipe = new Recipe(id);
@@ -76,7 +79,9 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             //Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
         } catch (err) {
             alert(err);
         }
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -34,6 +34,7 @@ export default class Recipe {
     parseIngredients() {
         const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const uint = [...unitsShort, 'g', 'kg']
 
         const newIngredients = this.ingredients.map(el => {
             //1. Uniform unities
@@ -47,7 +48,7 @@ export default class Recipe {
 
             //3. Parse igredients into count, unity, and ingredient
             const arrIng = ingredient.split(' ');
-            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
             let objIng;
             if (unitIndex > -1) {
